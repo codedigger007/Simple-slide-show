a2945924637f5f434536b7b7b5b3f36183186ea6
ELOCHUKWU <95888035+codedigger007@users.noreply.github.com>
2022-09-22 18:45:21 +0100

    Delete testingSome.js

--- testingSome.js
+++
@@ -1,7 +0,0 @@
-
-function openPops() {
-    console.log("Clicked")
-    window.open("final.html", "About Us", "width=350, heigth=400")
-    let b = window.open("final.html", "About Us", "width=350, heigth=400")
-
-}
